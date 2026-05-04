acd5487553b1de0c9934f7fe34c5bcdecc5f8665
Fergany71 <omarfergany100@gmail.com>
2026-05-04 03:13:12 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
+
+  // --- ربط الإشعارات ---
+  OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+  OneSignal.initialize("1d3faf09-9e8a-4975-b29f-cb0063b21568"); 
+  OneSignal.Notifications.requestPermission(true);
+
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyWebView(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyWebView extends StatefulWidget {
+  const MyWebView({super.key});
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Mr Ahmed Gamal',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
-    );
-  }
+  State<MyWebView> createState() => _MyWebViewState();
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+class _MyWebViewState extends State<MyWebView> {
+  InAppWebViewController? webViewController;
 
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black, // خلفية سوداء عشان تليق مع المنصة
       body: SafeArea(
-        child: InAppWebView(
-          initialUrlRequest: URLRequest(
-            url: Uri.parse("https://ahmedgamalplatform.omarelfergany9.workers.dev/"),
+        child: WillPopScope(
+          onWillPop: () async {
+            if (webViewController != null && await webViewController!.canGoBack()) {
+              webViewController!.goBack(); // عشان زرار الرجوع في الموبايل يرجع صفحة ورا في الموقع
+              return false;
+            }
+            return true;
+          },
+          child: InAppWebView(
+            initialUrlRequest: URLRequest(
+              url: WebUri("https://ahmedgamalplatform.omarelfergany9.workers.dev/"),
+            ),
+            initialSettings: InAppWebViewSettings(
+              javaScriptEnabled: true,
+              allowsInlineMediaPlayback: true, // عشان الفيديوهات تشتغل جوه الصفحة
+              useHybridComposition: true,
+            ),
+            onWebViewCreated: (controller) {
+              webViewController = controller;
+            },
           ),
-          initialOptions: InAppWebViewGroupOptions(
-            crossPlatform: InAppWebViewOptions(
+        ),
+      ),
+    );
+  }
+}
               javaScriptEnabled: true,
               transparentBackground: true, 
             ),
